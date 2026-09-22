3057d315ab3a2d78810c36e7632e63adb105be54
Anzela <nikitchenkoangela38@gmail.com>
2026-09-22 21:11:03 +0300

    Зміни

--- Team-Project_Voting/Setting.Designer.cs
+++ Team-Project_Voting/Setting.Designer.cs
@@ -28,52 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
             button1 = new Button();
             button2 = new Button();
-            dateTimePicker1 = new DateTimePicker();
-            label3 = new Label();
-            dateTimePicker2 = new DateTimePicker();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = SystemColors.GradientActiveCaption;
-            label1.Location = new Point(30, 88);
-            label1.Name = "label1";
-            label1.Size = new Size(127, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Початок голосування";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = SystemColors.ActiveCaption;
-            label2.Location = new Point(275, 29);
-            label2.Name = "label2";
-            label2.Size = new Size(195, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Рядок підключення до бази даних:";
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = Color.FromArgb(128, 255, 255);
-            textBox1.Location = new Point(275, 88);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(195, 23);
-            textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(621, 80);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(138, 23);
-            textBox2.TabIndex = 3;
-            textBox2.Text = "Зберегти голосування";
             // 
             // button1
             // 
@@ -93,44 +53,42 @@
             button2.Text = "Очистити голосування";
             button2.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // button3
             // 
-            dateTimePicker1.Location = new Point(30, 149);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(200, 23);
-            dateTimePicker1.TabIndex = 6;
+            button3.Location = new Point(621, 56);
+            button3.Name = "button3";
+            button3.Size = new Size(138, 48);
+            button3.TabIndex = 6;
+            button3.Text = "Зберегти голосування";
+            button3.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // textBox1
             // 
-            label3.AutoSize = true;
-            label3.BackColor = SystemColors.GradientActiveCaption;
-            label3.Location = new Point(30, 225);
-            label3.Name = "label3";
-            label3.Size = new Size(116, 15);
-            label3.TabIndex = 7;
-            label3.Text = "Кінець голосування";
+            textBox1.Location = new Point(89, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(155, 23);
+            textBox1.TabIndex = 7;
+            textBox1.Text = "Назва голосування";
             // 
-            // dateTimePicker2
+            // textBox2
             // 
-            dateTimePicker2.Location = new Point(30, 299);
-            dateTimePicker2.Name = "dateTimePicker2";
-            dateTimePicker2.Size = new Size(200, 23);
-            dateTimePicker2.TabIndex = 8;
+            textBox2.Location = new Point(89, 162);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(155, 72);
+            textBox2.TabIndex = 8;
+            textBox2.Text = "Варіанти відповіді";
             // 
             // Setting
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(dateTimePicker2);
-            Controls.Add(label3);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(button2);
-            Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Setting";
             Text = "Setting";
             ResumeLayout(false);
@@ -138,15 +96,10 @@
         }
 
         #endregion
-
-        private Label label1;
-        private Label label2;
-        private TextBox textBox1;
-        private TextBox textBox2;
         private Button button1;
         private Button button2;
-        private DateTimePicker dateTimePicker1;
-        private Label label3;
-        private DateTimePicker dateTimePicker2;
+        private Button button3;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
